3156d2506eed994b00f28741b2c91595b51166ba
wcwaterbender <samcornish96@gmail.com>
2019-01-20 17:33:37 -0800

    just need bg processes to work now

--- simple_shell.cpp
+++ simple_shell.cpp
@@ -16,6 +16,7 @@ using namespace std;
 
 static char* args[512];
 pid_t pid;
+int status;
 int command_pipe[2];
 static int n = 0;
 static char line1[1024];
@@ -62,7 +63,7 @@ static void split(char* cmd)
 		cmd = skipwhite(next + 1);
 		next = strchr(cmd, ' ');
 	}
- 
+ 	//if command is not empty
 	if (cmd[0] != '\0') {
 		args[i] = cmd;
 		next = strchr(cmd, '\n');
@@ -71,36 +72,46 @@ static void split(char* cmd)
 	}
  
 	args[i] = NULL;
+	
 }
 
 //run the command
-static int command(int input,int output, int first, int last)
+static int command(int input,int output, int first, int last, int bg, int redir)
 {
 	int pipettes[2];
  
-	pipe( pipettes );	
+	pipe( pipettes );
+		
 	pid = fork();
- 
-	if (pid == 0) {
+
+	if (pid == 0) {//child
 		if (first == 1 && last == 0 && input == 0) {
 			// First command
+			dup2( pipettes[WRITE], STDOUT_FILENO );
+		} else if (redir != 0) {
+			dup2(input, STDIN_FILENO);
 			dup2( pipettes[WRITE], STDOUT_FILENO );
 		} else if (first == 0 && last == 0 && input != 0) {
 			// middle commands
 			dup2(input, STDIN_FILENO);
 			dup2(pipettes[WRITE], STDOUT_FILENO);
-		}else if (last ==1  && output != 0){//last command with output redir
-		       dup2( input, 0);
-		       dup2(output, 1);
+		} else if (last ==1  && output != 0){//last command with output redir
+		   	dup2( input, 0);
+		       	dup2(output, 1);
 		} else {
 			// last command
 			dup2( input, STDIN_FILENO );
 		}
- 
 		if (execvp( args[0], args) == -1)
 			_exit(EXIT_FAILURE); // If child fails
 	}
- 
+	else{//parent
+		if(!(bg)){
+			waitpid(pid,&status,WUNTRACED);
+		}else{
+			signal(SIGCHLD,func);
+		}
+	}
 	if (input != 0) 
 		close(input);
  
@@ -121,14 +132,14 @@ static void cleanup(int n)
 		wait(NULL);
 }
 
-static int run(char* cmd, int input, int output, int first, int last)
+static int run(char* cmd, int input, int output, int first, int last, int bg, int redir)
 {
 	split(cmd);
 	if (args[0] != NULL) {
 		if (strcmp(args[0], "exit") == 0) 
 			exit(0);
 		n += 1;
-		return command(input,output, first, last);
+		return command(input,output, first, last, bg, redir);
 	}
 	return 0;
 }
@@ -161,14 +172,26 @@ void shell_loop(int flag){
 			printf("shell: ");
 
 		fgets(line1,BUFFERSIZE,stdin);
+		int bgFlag = 0;
+		//check bg process and strip the &
+		if(strchr(line1,'&') != NULL){
+			bgFlag = 1;
+			//trim off the &
+			line1[strlen(line1)-2] = 0;
+		}
 		line = clean_input(line1);
 		char* cmd = line;
 		char *inputFile = NULL;
 		char *outputFile = NULL;
+		char rebuild[1024];
 		char* inputRedir = strchr(cmd, '<');
 		char* outputRedir = strchr(cmd, '>');
-	        int input, output;	
-		int bgFlag = 0;
+		char* pipeLoc = strchr(cmd,'|');
+		
+	        int input, output;
+		int redir = 0;	
+		
+
 
 		if(inputRedir!=NULL){
  			*inputRedir = '\0';
@@ -178,10 +201,18 @@ void shell_loop(int flag){
 				exit(-1);
 			}
  			input = open(inputFile, O_RDONLY);
+			if(pipeLoc != NULL){
+				strcpy(rebuild,cmd);
+				strcat(rebuild,pipeLoc);
+				cmd = rebuild;
+			}
  		}
-		else
+		else{
  			input = 0;
-
+		}
+		
+			
+		
 		if(outputRedir !=NULL) {
 			*outputRedir = '\0';
  			outputFile = strtok(outputRedir + 1, " \t\n");
@@ -190,28 +221,26 @@ void shell_loop(int flag){
  		else
  			output = 0;
 		
-		//check bg process and strip the &
-		if(strchr(line,'&') != NULL){
-			bgFlag = 1;
-			//trim off the &
-			line[strlen(line)-2] = 0;
-		}
+		
 		int first = 1;
 
-		
+			
 		char* next = strchr(cmd, '|');
-
+		if(next != NULL){
+			redir = 1;
+		}
 	        //go through command list	
 		while (next != NULL) {
 			*next = '\0';
-			input = run(cmd, input, output, first, 0);
+			input = run(cmd, input, output, first, 0, bgFlag, redir);
  
 			cmd = next + 1;
 			next = strchr(cmd, '|'); // Find next '|' if present
 			first = 0;
+			redir = 0;
 		}
 		first = 0;
-		input = run(cmd, input, output, first, 1);
+		input = run(cmd, input, output, first, 1, bgFlag,redir);
 	
 		if (input!=0){
 			close(input);
